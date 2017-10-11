69125fee34644d26ea87b4d865374713041e23f7
Pablo Daniel Rey <pablodanielrey@gmail.com>
2017-10-11 12:54:26 -0300

    cambie para que se ejecute en segundo plano

--- docker/ejecutar.sh
+++ docker/ejecutar.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run -ti --name google -p 5035:5000 -p 5036:5001 --rm --env-file $HOME/gitlab/fce/produccion/google google
+sudo docker run -d --name google -p 5035:5000 -p 5036:5001 --rm --env-file $HOME/gitlab/fce/produccion/google google
