5a139dbc73bce3118465ddda248a90f843be1f1e
Pablo Daniel Rey <pablodanielrey@gmail.com>
2017-10-11 13:06:41 -0300

    me faltaba el volumen con el codigo del sistema

--- docker/ejecutar.sh
+++ docker/ejecutar.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-sudo docker run -d --name google -p 5035:5000 -p 5036:5001 --name google --rm --env-file $HOME/gitlab/fce/produccion/google google
+sudo docker run -d --name google -v $(pwd)/src:/src -p 5035:5000 -p 5036:5001 --name google --rm --env-file $HOME/gitlab/fce/produccion/google google
+sudo docker exec -t google bash instalar.sh
+sudo docker restart google
+
