5d949dea6d7c3dbf3f851dde1b469a14debff197
Pablo Daniel Rey <pablodanielrey@gmail.com>
2017-10-26 13:02:15 -0300

    family name es el apellido!!!

--- docker/src/google/model/GoogleModel.py
+++ docker/src/google/model/GoogleModel.py
@@ -195,7 +195,7 @@ class GoogleModel:
                     # datos a actualizar
                     datos = {}
 
-                    datos["name"] = {"familyName": user["nombre"], "givenName": user["apellido"], "fullName": fullName}
+                    datos["name"] = {"familyName": user["apellido"], "givenName": user["apellido"], "fullName": fullName}
 
                     r = service.users().update(userKey=userGoogle,body=datos).execute()
                     ds = cls._crearLog(r)
