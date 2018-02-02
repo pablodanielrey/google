6b3f146764ed3f1ff478ae4ed42657c7f76add91
Pablo Daniel Rey <pablodanielrey@gmail.com>
2018-02-02 12:27:35 -0300

    cambiamos la estructura para manejar el tema de la web

--- docker/ejecutar.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo docker run -d --name google -v $(pwd)/src:/src -p 5035:5000 -p 5036:5001 --name google --rm --env-file $HOME/gitlab/fce/produccion/google google
-sudo docker exec -t google bash instalar.sh
-sudo docker restart google
-
